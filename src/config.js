0b9c7f7cf1ad6f3d6a399893d2524d346d0e798f
Viet Duc <ducthichcode@gmail.com>
2024-04-30 14:01:40 +0700

    update code

--- src/config.js
+++
@@ -1,6 +0,0 @@
-// import { config } from "dotenv";
-
-// config();
-
-// export const MONGODB_URI =
-//   process.env.MONGODB_URI || "mongodb+srv://ducvietdev:vietduc24122002@testdeploy.1tezoqa.mongodb.net/?retryWrites=true&w=majority&appName=TestDeploy";
